ee7545903df72c83e1783cbf87a20968e2faed4c
EOMZON <116791118+EOMZON@users.noreply.github.com>
2023-11-13 20:09:23 +0800

    20231113 style show fix

--- next.config.js
+++ next.config.js
@@ -1,17 +1,16 @@
 /**
  * @type {import('next').NextConfig}
  */
+
+const repo = 'change-me-to-your-repo';
+const assetPrefix = `/${repo}/`;
+const basePath = `/${repo}`;
+
 const nextConfig = {
   output: 'export',
 
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
-
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
-
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
+  assetPrefix: assetPrefix,
+  basePath: basePath,
 };
 
 module.exports = nextConfig;
