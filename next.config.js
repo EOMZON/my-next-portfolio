12620a7b1dba05ad83a1c01c75b75f4918567965
EOMZON <116791118+EOMZON@users.noreply.github.com>
2023-11-14 11:21:48 +0800

    20231114 static deploy

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
 
-  // Add basePath
-  basePath: '/my-next-portfolio',
+const repo = 'my-next-portfolio';
+const assetPrefix = `/${repo}/`;
+const basePath = `/${repo}`;
+
+const nextConfig = {
+  basePath,
+  assetPrefix,
   output: 'export',
 };
 
